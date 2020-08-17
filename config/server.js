4e09be0ffde39a3241f56f8805aa94a5f5206832
Thiago Airton Demeterko <thiago.demeterko@gmail.com>
2020-08-17 20:50:44 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2009ff67fa323823e358d18eea93b3e5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '909b4bce-7bd6-4cce-b19f-4dd8432fe64f'
+};
